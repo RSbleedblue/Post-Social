0a087ca66d6b32a0afbf135d8ecaea35484e5497
Rivansh <rivansh63@gmail.com>
2024-03-04 22:05:54 +0530

    UI changes in PostCards

--- FrontEnd/src/Components/Home/sub-Components/PostCard.jsx
+++ FrontEnd/src/Components/Home/sub-Components/PostCard.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from '../../../assets/Logo/SlackPic.png';
-import testPhoto from '../../../assets/Logo/Test1.jpg';
-import { BsThreeDots } from 'react-icons/bs';
+import React from "react";
+import logo from "../../../assets/banner.jpg";
+import testPhoto from "../../../assets/Logo/Test1.jpg";
+import { BsThreeDots } from "react-icons/bs";
 import { FaHeart } from "react-icons/fa";
 import { FaCommentAlt } from "react-icons/fa";
 import { FaShareAlt } from "react-icons/fa";
@@ -9,24 +9,45 @@ import { FaShareAlt } from "react-icons/fa";
 export default function PostCard() {
   return (
     <>
-      <div className='flex flex-row gap-4 items-center'>
-        <img src={logo} alt='Image' className='rounded-full h-16' />
-        <span className='text-pmpurple text-2xl font-semibold'>Rivansh Srivastava</span>
-        <BsThreeDots className='text-pmpurple ml-auto text-2xl cursor-pointer' />
+      <div className="flex flex-row gap-4 items-center">
+        <div className="rounded-full overflow-hidden max-h-[50px] max-w-[50px]">
+          <img src={logo} alt="Image" className="w-full h-full object-cover" />
+        </div>
+        <span className="text-pmpurple text-2xl font-semibold">
+          Rivansh Srivastava
+        </span>
+        <BsThreeDots className="text-pmpurple ml-auto text-2xl cursor-pointer" />
       </div>
-      <p className='p-4 text-l text-pmpurple opacity-90'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis, fugit! Maxime esse accusamus quia laudantium placeat corrupti pariatur, magnam eligendi.</p>
-      <div className='text-xl flex '>
-        <div className='max-w-screen-lg mx-auto'>
-          <img src={testPhoto} className='rounded-lg w-full max-h-[300px] max-w-[600px]' alt='Test Photo' />
+      <p className="p-3 text-l text-pmpurple opacity-90">
+        Lorem, ipsum 
+      </p>
+      <div className="text-xl flex w-full justify-center">
+        <div className="max-w-screen-lg mx-auto rounded-lg overflow-hidden">
+          <img
+            src={testPhoto}
+            className="w-full h-full object-cover"
+            alt="Test Photo"
+          />
         </div>
       </div>
-      <div className='flex p-4 m-4 flex-row '>
-            <FaHeart className='text-pmpurple text-2xl hover:text-hoverLike mb-2 cursor-pointer'/>
-            <span className='text-pmpurple mr-10 ml-2'>10k</span>
-            <FaCommentAlt className='text-pmpurple text-2xl hover:text-hoverLike cursor-pointer'/>
-            <span className='text-pmpurple mr-10 ml-2'>2256</span>
-            <FaShareAlt className='text-pmpurple text-2xl cursor-pointer'/>
+      <div className="flex p-2 m-2 flex-row ">
+        <FaHeart className="text-pmpurple text-2xl hover:text-hoverLike mb-2 cursor-pointer" />
+        <span className="text-pmpurple mr-10 ml-2">10k</span>
+        <FaCommentAlt className="text-pmpurple text-2xl hover:text-hoverLike cursor-pointer" />
+        <span className="text-pmpurple mr-10 ml-2">2256</span>
+        <FaShareAlt className="text-pmpurple text-2xl cursor-pointer" />
+      </div>
+      <div className="flex p-2 m-2 flex-row gap-1 items-center">
+        <span className="text-pmpurple "> Description : Lorem ipsum dolor sit amet consectetur</span>
+      </div>
+      <div className="flex flex-row gap-1 items-center">
+        <div className="rounded-full overflow-hidden max-h-[30px] max-w-[30px]">
+          <img src={logo} alt="Image" className="w-full h-full object-cover" />
         </div>
+        <span className="text-pmpurple text-xs opacity-50">
+          Add Comment....
+        </span>
+      </div>
     </>
   );
 }
--- FrontEnd/src/Components/Home/sub-Components/RSideFeed.jsx
+++ FrontEnd/src/Components/Home/sub-Components/RSideFeed.jsx
@@ -6,7 +6,7 @@ function RSideFeed() {
     <div className="rounded-lg text-black h-[285px] flex-1 flex flex-col pl-6 justify-between relative">
       <div className="bg-pmpurple w-full rounded-xl h-[140px] relative "></div>
       <div className="bg-white w-[95%] rounded-xl h-[140px] pl-32 text-xl">
-        Pratik
+        Rivansh Srivastava
       </div>
       <div className="bg-white w-[160px] rounded-full h-[160px] absolute -translate-y-1/2 top-1/2 -left-4 border-bgmain border-8 overflow-hidden">
         <img src={luffy} alt="" />
