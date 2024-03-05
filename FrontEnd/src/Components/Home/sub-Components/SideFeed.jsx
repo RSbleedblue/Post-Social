402b4e0ca59aa887bf43cca3de739d8f972b493e
Rivansh <rivansh63@gmail.com>
2024-03-05 10:27:20 +0530

    Adding files to TestBranch

--- FrontEnd/src/Components/Home/sub-Components/SideFeed.jsx
+++ FrontEnd/src/Components/Home/sub-Components/SideFeed.jsx
@@ -21,7 +21,7 @@ function SideFeed() {
           <p className="text-sm font-semibold">Posts</p>
         </div>
         <div className="rounded-lg flex items-center p-4 hover:text-white hover:bg-pmpurple transition-all duration-100 cursor-pointer">
-          <FaUserFriends className="h-4 sm:h-7 w-[17%] pr-2" />
+          <FaUserFriends className="h-4 sm:h-7 w-[17%] pr-2" /> 
           <p className="text-sm font-semibold">Friends</p>
         </div>
       </div>
