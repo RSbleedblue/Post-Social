efa0a17686bd1b054412f9c9ddb3b422876d7c04
Pratik Jadon <pratikucskmpj@gmail.com>
2024-03-06 17:05:00 +0530

    RsideFeed unifying

--- FrontEnd/src/Components/Home/sub-Components/RSideFeed.jsx
+++ FrontEnd/src/Components/Home/sub-Components/RSideFeed.jsx
@@ -1,40 +1,49 @@
 import React from "react";
 import luffy from "../../../assets/banner.jpg";
 import prof_banner from "../../../assets/profile_banner.jpeg";
+import { FaRegUser } from "react-icons/fa";
 
 function RSideFeed() {
   return (
-    <div className="rounded-lg text-black h-[480px] flex-1 flex flex-col pl-6 justify-between relative">
-      <div className="w-full rounded-xl min-h-[100px] relative overflow-hidden">
+    <div className="flex-1 flex-col relative h-[72%]">
+      <div className="w-full h-[40%] rounded-lg over overflow-hidden">
         <img
           src={prof_banner}
           className="w-full h-full object-cover opacity-80"
         />
       </div>
-      <div className="flex flex-col items-center bg-white w-[100%] rounded-xl min-h-[140px] h-full text-xl">
-        <p className="ml-4 mt-6 pt-2 font-semibold text-center">
-          Rivansh Srivastava
-        </p>
-        <p className="w-[70%] text-sm  pt-6 pb-4 mx-auto font-thin text-justify ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam veniam
-          pariatur atque perferendis aut enim
-        </p>
-        <div className="flex justify-evenly w-full">
-          <div className="flex flex-col items-center w-[10%]">
-            <p className="font-semibold text-lg">1K</p>
-            <p className="font-thin text-sm">Post</p>
+      <div className="w-full bg-white h-[60%] rounded-lg">
+        <div className="pt-8 text-pmpurple font-semibold text-xl flex justify-center">
+          Rivansh
+        </div>
+        <div className="font-thin px-10 text-justify pt-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+          eveniet cumque accusamus impedit, quaerat pariatur dicta culpa
+          distinctio. Quam, iste.
+        </div>
+        <div className="w-full pt-5 p-4 flex justify-center gap-24">
+          <div className="flex flex-col items-center">
+            <p className="text-pmpurple font-semibold text-xl">1k</p>
+            <p className="font-thin">Likes</p>
           </div>
-          <div className="flex flex-col items-center w-[10%]">
-            <p className="font-semibold text-lg">1560</p>
-            <p className="font-thin text-sm">Likes</p>
+          <div className="flex flex-col items-center">
+            <p className="text-pmpurple font-semibold text-xl">1560</p>
+            <p className="font-thin">Friends</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text-pmpurple font-semibold text-xl">10</p>
+            <p className="font-thin">Posts</p>
           </div>
         </div>
-        <button className="w-1/3 mt-2 p-2 bg-pmpurple text-white rounded-xl shadow-2xl text-sm font-semibold ">
-          View Profile
-        </button>
+        <div className="w-full flex justify-center">
+          <button className="flex justify-center items-center gap-2 w-[40%] bg-pmpurple p-2 rounded-xl text-white hover:drop-shadow-xl hover:bg-white hover:text-pmpurple transition-all duration-100">
+            View Profile
+            <FaRegUser />
+          </button>
+        </div>
       </div>
-      <div className="bg-white w-[120px] rounded-full h-[120px] absolute -translate-y-1/2 top-[38%] -left-1 border-bgmain border-8 overflow-hidden">
-        <img src={luffy} alt="" />
+      <div className="w-[110px] h-[110px] bg-pmpurple absolute overflow-hidden rounded-full top-[42%] -translate-y-1/2 -translate-x-4 border-8 border-bgmain">
+        <img src={luffy} />
       </div>
     </div>
   );
